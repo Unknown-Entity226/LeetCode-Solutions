3df3d534cd9f36bcc277acca1bc854515a401a6c
Aarav Gupta <forpcandfoxxy123@gmail.com>
2026-02-21 00:16:36 +0530

    Adds 2235_add_two_numbers.py
    Adds a function to add up two integers

---
+++ maths/2235_add_two_numbers.py
@@ -0,0 +1,24 @@
+'''
+Problem Description:
+Given two integers num1 and num2,
+return their sum.
+
+Approach:
+- Directly return the sum using the + operator.
+- No special handling is required since integers
+  are directly supported in Python.
+
+Time Complexity:
+O(1)
+
+Space Complexity:
+O(1)
+'''
+class Solution(object):
+    def sum(self, num1, num2):
+        """
+        :type num1: int
+        :type num2: int
+        :rtype: int
+        """
+        return num1 + num2
